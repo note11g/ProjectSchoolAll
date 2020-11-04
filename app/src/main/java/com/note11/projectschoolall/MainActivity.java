d8ff45cf6253b3c377caa546fcc7f20620ad29f0
note11 <67783062+note11g@users.noreply.github.com>
2020-11-04 23:28:21 +0900

    Initial commit

---
+++ app/src/main/java/com/note11/projectschoolall/MainActivity.java
@@ -0,0 +1,14 @@
+package com.note11.projectschoolall;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
